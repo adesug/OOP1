75b234b7610917b40fa55b93d201b0710620866a
adesug <56187116+adesug@users.noreply.github.com>
2020-03-13 14:23:38 +0700

    Add files via upload

---
+++ TugasOOP1/src/Tugas1/Mahasiswa.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Tugas1;
+
+/**
+ *
+ * @author USER
+ */
+public class Mahasiswa {
+    
+    public static void main(String[] args) {
+        String nim1 = "18090064";
+        String nama1 = "Ade Sugiantoro";
+        String Kelas1 = "4C";
+       
+        System.out.println("Nim : " +nim1 );
+        System.out.println("Nama : " +nama1);
+        System.out.println("Kelas : "+ Kelas1);
+        
+        System.out.println();
+    }
+}
